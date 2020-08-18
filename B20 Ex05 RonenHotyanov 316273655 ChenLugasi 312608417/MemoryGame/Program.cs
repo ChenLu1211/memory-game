a2ce70c8ddff421331749cbcd04cf89aa708cbb9
Chen Lu <59344011+ChenLu1211@users.noreply.github.com>
2020-08-18 22:56:08 +0300

    Add files via upload

---
+++ B20 Ex05 RonenHotyanov 316273655 ChenLugasi 312608417/MemoryGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace MemoryGame
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            FormLogin loginProperties = new FormLogin();
+        }
+    }
+}
